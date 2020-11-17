05430c4992d5eb92d0d91866ce1c2c38d0f7932b
Dmitry <dmitrydemin1973@gmail.com>
2020-11-17 21:31:52 +0700

    ADD retunrcode oracle error

--- run_all_sql_dir.py
+++ run_all_sql_dir.py
@@ -8,11 +8,14 @@ NLS_DATE_FORMAT = "\'DD.MM.YYYY HH24:MI:SS\'"
 NLS_NUMERIC_CHARACTERS = "\'.,\'"
 AUTO_COMMIT = "OFF"
 NLS_LANG = 'AMERICAN_AMERICA.CL8MSWIN1251'
+WHENEVER = 'WHENEVER SQLERROR EXIT SQL.SQLCODE'
+#WHENEVER = 'WHENEVER SQLERROR CONTINUE'
 # silent sqlplus = "-s"  silent off sqlplus = ""
 silent_sqlplus = ""
 
-Sqlheader = """
-set autocommit """ + AUTO_COMMIT + "\n" + """set define off
+Sqlheader = WHENEVER + """\n
+set autocommit """ + AUTO_COMMIT + "\n" + \
+"""set define off 
 set timing on
 set linesize 10000
 set pagesize 10000
@@ -55,7 +58,7 @@ password = options.password
 connectstring = options.connectstring
 logfilename = options.logfilename
 
-logging.basicConfig(format='%(asctime)s %(levelname)s  %(message)s', filename=logfilename, level=logging.INFO)
+logging.basicConfig(format='%(asctime)s %(levelname)s  %(message)s', filename=logfilename, level=logging.DEBUG)
 
 logging.info('Start  script ')
 
@@ -79,6 +82,7 @@ logging.info("Set AUTO_COMMIT="+AUTO_COMMIT)
 logging.info("Set NLS_LANG=" + NLS_LANG)
 logging.info("Set NLS_DATE_FORMAT=" + NLS_DATE_FORMAT )
 logging.info("Set NLS_NUMERIC_CHARACTERS=" + NLS_NUMERIC_CHARACTERS)
+logging.info('Set ' + WHENEVER)
 
 connectoracle = str(username) + "/" + str(password) + '@' + str(connectstring)
 
@@ -87,7 +91,13 @@ sqlplus_result = subprocess.run([r'sqlplus.exe', silent_sqlplus, connectoracle],
                                 stderr=subprocess.PIPE, stdout=subprocess.PIPE, text=True, input=SqlQueryALL)
 
 #print(sqlplus_result.stdout)
+#print(sqlplus_result.returncode)
 
 logging.info(sqlplus_result.stdout)
+
+if sqlplus_result.returncode > 0:
+    ORACLE_ERROR = "Oracle error ORA-" + str(sqlplus_result.returncode)
+    print(ORACLE_ERROR)
+    logging.error(ORACLE_ERROR)
 
 logging.info('Stop  script')
