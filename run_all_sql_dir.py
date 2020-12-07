022b32d19bedb98fd5b7d7959dd5631786545328
Dmitry <dmitrydemin1973@gmail.com>
2020-12-07 21:21:21 +0700

    add only make summary sql file

--- run_all_sql_dir.py
+++ run_all_sql_dir.py
@@ -8,11 +8,14 @@ NLS_DATE_FORMAT = "\'DD.MM.YYYY HH24:MI:SS\'"
 NLS_NUMERIC_CHARACTERS = "\'.,\'"
 AUTO_COMMIT = "OFF"
 NLS_LANG = 'AMERICAN_AMERICA.CL8MSWIN1251'
+WHENEVER = 'WHENEVER SQLERROR EXIT SQL.SQLCODE'
+#WHENEVER = 'WHENEVER SQLERROR CONTINUE'
 # silent sqlplus = "-s"  silent off sqlplus = ""
 silent_sqlplus = ""
 
-Sqlheader = """
-set autocommit """ + AUTO_COMMIT + "\n" + """set define off
+Sqlheader = WHENEVER + """\n
+set autocommit """ + AUTO_COMMIT + "\n" + \
+"""set define off 
 set timing on
 set linesize 10000
 set pagesize 10000
@@ -48,14 +51,20 @@ parser.add_option("-l", "--log", dest="logfilename",
                   default=None,
                   help="Name of log file ")
 
+parser.add_option("-s", "--sql", dest="sqlfilename",
+                  default=None,
+                  help="Name of summary SQL file ")
+
+
 (options, args) = parser.parse_args()
 input_dir_sql = options.dirname
 username = options.username
 password = options.password
+sqlfilename = options.sqlfilename
 connectstring = options.connectstring
 logfilename = options.logfilename
 
-logging.basicConfig(format='%(asctime)s %(levelname)s  %(message)s', filename=logfilename, level=logging.INFO)
+logging.basicConfig(format='%(asctime)s %(levelname)s  %(message)s', filename=logfilename, level=logging.DEBUG)
 
 logging.info('Start  script ')
 
@@ -72,22 +81,34 @@ for currentFile in currentDirectory.iterdir():
 
 SqlQueryALL = Sqlheader + sqlscript
 logging.debug("Sqlfile:\n" + SqlQueryALL)
-
 my_env = os.environ.copy()
 my_env["NLS_LANG"] = NLS_LANG
 logging.info("Set AUTO_COMMIT="+AUTO_COMMIT)
 logging.info("Set NLS_LANG=" + NLS_LANG)
 logging.info("Set NLS_DATE_FORMAT=" + NLS_DATE_FORMAT )
 logging.info("Set NLS_NUMERIC_CHARACTERS=" + NLS_NUMERIC_CHARACTERS)
+logging.info('Set ' + WHENEVER)
 
 connectoracle = str(username) + "/" + str(password) + '@' + str(connectstring)
 
-logging.info("Connect ro oracle:" + connectoracle)
-sqlplus_result = subprocess.run([r'sqlplus.exe', silent_sqlplus, connectoracle], env=my_env,
+if username is not None and password is not None:
+
+    logging.info("Connect ro oracle:" + connectoracle)
+    sqlplus_result = subprocess.run([r'sqlplus.exe', silent_sqlplus, connectoracle], env=my_env,
                                 stderr=subprocess.PIPE, stdout=subprocess.PIPE, text=True, input=SqlQueryALL)
 
-#print(sqlplus_result.stdout)
+    #print(sqlplus_result.stdout)
+    #print(sqlplus_result.returncode)
 
-logging.info(sqlplus_result.stdout)
+    logging.info(sqlplus_result.stdout)
 
+    if sqlplus_result.returncode > 0:
+         ORACLE_ERROR = "Oracle error ORA-" + str(sqlplus_result.returncode)
+         print(ORACLE_ERROR)
+         logging.error(ORACLE_ERROR)
+elif sqlfilename is not None:
+    filedescriptor = open(str(sqlfilename), 'w')
+    filedescriptor.write(SqlQueryALL + '\n')
+    filedescriptor.close()
+    logging.info( 'SQL script: ' + str(sqlfilename) + ' was created.')
 logging.info('Stop  script')
